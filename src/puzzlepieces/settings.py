2e4d89051fa3e4b5a3303b173ba5375fda853457
Fabian Bäumer <fabianbaeumer@gmail.com>
2020-01-19 23:15:32 +0100

    Allow a connection to persist for 60 seconds

--- src/puzzlepieces/settings.py
+++ src/puzzlepieces/settings.py
@@ -88,6 +88,7 @@ DATABASES = {
         "PASSWORD": os.environ.get("SQL_PASSWORD", "puzzling"),
         "HOST": os.environ.get("SQL_HOST", "localhost"),
         "PORT": os.environ.get("SQL_PORT", "5432"),
+        "CONN_MAX_AGE": 60,
         "OPTIONS": {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         },
